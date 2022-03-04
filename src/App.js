b2f07bcc1aa1e07c96d6311a3dd719cc9837cac6
Devam Trivedi <devam.trivedi@bfhl.in>
2022-03-04 13:27:12 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const grid=(word)=>{
+    const len=word.length
+    return <>{new Array(6).fill(1).map((e,i)=><div className='flex' key={i}>
+      {new Array(len).fill(1).map((e,j)=><div className='box' key={j}></div>)}
+    </div>)}</>
+  }
+  return <div className='App'>{grid("Happy")}</div>
 }
 
 export default App;
